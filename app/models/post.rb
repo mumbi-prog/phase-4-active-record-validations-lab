110a561368d23736b571fef46069f105442ba182
Sylvia Mumbi <mumbi099@gmail.com>
2023-08-17 03:56:54 +0300

    Done lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  validate :click_baity
+
+  def click_baity
+    return if title.blank?
+    clickbait_phrases = ["Won't Believe", 'Secret', 'Top [number]', 'Guess']
+    title_contains_clickbait = clickbait_phrases.any? { |phrase| title.include?(phrase) }
+    errors.add(:title, 'Not clickbaity') unless title_contains_clickbait
+  end
 end
